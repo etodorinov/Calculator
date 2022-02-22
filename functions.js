3dd56fd6e30ae75b0b7ace776f2551802a514eb8
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-02-22 07:29:23 +0200

    Event listeners added

---
+++ functions.js
@@ -0,0 +1,84 @@
+const screenElement = document.getElementById("screen");
+const resetButtonElement = document.getElementById("reset");
+const plusButtonElement = document.getElementById("plus");
+const minusButtonElement = document.getElementById("minus");
+const multiplyButtonElement = document.getElementById("multiply");
+const deleteButtonElement = document.getElementById("division");
+const equalButtonElement = document.getElementById("equal");
+const percentButtonElement = document.getElementById("percent");
+const squareRootButtonElement = document.getElementById("square-root");
+const dotButtonElement = document.getElementById("dot");
+const doubleZeroButtonElement = document.getElementById("double_zero");
+const zeroButtonElement = document.getElementById("zero");
+const oneButtonElement = document.getElementById("one");
+const twoButtonElement = document.getElementById("two");
+const threeButtonElement = document.getElementById("three");
+const fourButtonElement = document.getElementById("four");
+const fiveButtonElement = document.getElementById("five");
+const sixButtonElement = document.getElementById("six");
+const sevenButtonElement = document.getElementById("seven");
+const eightButtonElement = document.getElementById("eight");
+const nineButtonElement = document.getElementById("nine");
+
+resetButtonElement.addEventListener("click", resetCalculator);
+plusButtonElement.addEventListener("click", addNumbers);
+minusButtonElement.addEventListener("click", subtractNumbers);
+multiplyButtonElement.addEventListener("click", multiplyNumbers);
+deleteButtonElement.addEventListener("click", deleteNumbers);
+equalButtonElement.addEventListener("click", showResult);
+percentButtonElement.addEventListener("click", percentCalculations);
+squareRootButtonElement.addEventListener("click", squareRootCalculations);
+dotButtonElement.addEventListener("click", dotPressed);
+doubleZeroButtonElement.addEventListener("click", doubleZeroPressed);
+zeroButtonElement.addEventListener("click", zeroPressed);
+oneButtonElement.addEventListener("click", onePressed);
+twoButtonElement.addEventListener("click", twoPressed);
+threeButtonElement.addEventListener("click", threePressed);
+fourButtonElement.addEventListener("click", fourPressed);
+fiveButtonElement.addEventListener("click", fivePressed);
+sixButtonElement.addEventListener("click", sixPressed);
+sevenButtonElement.addEventListener("click", sevenPressed);
+eightButtonElement.addEventListener("click", eightPressed);
+nineButtonElement.addEventListener("click", ninePressed);
+
+function resetCalculator(e) {}
+function addNumbers(e) {}
+function subtractNumbers(e) {}
+function multiplyNumbers(e) {}
+function deleteNumbers(e) {}
+function showResult(e) {}
+function percentCalculations(e) {}
+function squareRootCalculations(e) {}
+function dotPressed(e) {}
+function doubleZeroPressed(e) {}
+function zeroPressed(e) {}
+function onePressed(e) {}
+function twoPressed(e) {}
+function threePressed(e) {}
+function fourPressed(e) {}
+function fivePressed(e) {}
+function sixPressed(e) {}
+function sevenPressed(e) {}
+function eightPressed(e) {}
+function ninePressed(e) {}
+
+console.log(screenElement);
+console.log(resetButtonElement);
+console.log(plusButtonElement);
+console.log(minusButtonElement);
+console.log(multiplyButtonElement);
+console.log(equalButtonElement);
+console.log(percentButtonElement);
+console.log(squareRootButtonElement);
+console.log(dotButtonElement);
+console.log(doubleZeroButtonElement);
+console.log(zeroButtonElement);
+console.log(oneButtonElement);
+console.log(twoButtonElement);
+console.log(threeButtonElement);
+console.log(fourButtonElement);
+console.log(fiveButtonElement);
+console.log(sixButtonElement);
+console.log(sevenButtonElement);
+console.log(eightButtonElement);
+console.log(nineButtonElement);
