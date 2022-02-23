ae169984e16d3967673063cc40bbaf6a321409ff
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-02-23 06:27:35 +0200

    Functionality added to number buttons

--- functions.js
+++ functions.js
@@ -20,6 +20,8 @@ const sevenButtonElement = document.getElementById("seven");
 const eightButtonElement = document.getElementById("eight");
 const nineButtonElement = document.getElementById("nine");
 
+screenElement.textContent = "0";
+
 resetButtonElement.addEventListener("click", resetCalculator);
 plusButtonElement.addEventListener("click", addNumbers);
 minusButtonElement.addEventListener("click", subtractNumbers);
@@ -41,7 +43,9 @@ sevenButtonElement.addEventListener("click", sevenPressed);
 eightButtonElement.addEventListener("click", eightPressed);
 nineButtonElement.addEventListener("click", ninePressed);
 
-function resetCalculator(e) {}
+function resetCalculator(e) {
+  screenElement.textContent = "0";
+}
 function addNumbers(e) {}
 function subtractNumbers(e) {}
 function multiplyNumbers(e) {}
@@ -50,17 +54,81 @@ function showResult(e) {}
 function percentCalculations(e) {}
 function squareRootCalculations(e) {}
 function dotPressed(e) {}
-function doubleZeroPressed(e) {}
-function zeroPressed(e) {}
-function onePressed(e) {}
-function twoPressed(e) {}
-function threePressed(e) {}
-function fourPressed(e) {}
-function fivePressed(e) {}
-function sixPressed(e) {}
-function sevenPressed(e) {}
-function eightPressed(e) {}
-function ninePressed(e) {}
+function doubleZeroPressed(e) {
+  if (screenElement.textContent !== "0" && screenElement.textContent !== "00") {
+    screenElement.textContent += "00";
+  }
+}
+function zeroPressed(e) {
+  if (screenElement.textContent === "0" && screenElement.textContent !== "00") {
+    screenElement.textContent = "0";
+  } else {
+    screenElement.textContent += "0";
+  }
+}
+function onePressed(e) {
+  if (screenElement.textContent === "0" && screenElement.textContent !== "00") {
+    screenElement.textContent = "1";
+  } else {
+    screenElement.textContent += "1";
+  }
+}
+function twoPressed(e) {
+  if (screenElement.textContent === "0" && screenElement.textContent !== "00") {
+    screenElement.textContent = "2";
+  } else {
+    screenElement.textContent += "2";
+  }
+}
+function threePressed(e) {
+  if (screenElement.textContent === "0" && screenElement.textContent !== "00") {
+    screenElement.textContent = "3";
+  } else {
+    screenElement.textContent += "3";
+  }
+}
+function fourPressed(e) {
+  if (screenElement.textContent === "0" && screenElement.textContent !== "00") {
+    screenElement.textContent = "4";
+  } else {
+    screenElement.textContent += "4";
+  }
+}
+function fivePressed(e) {
+  if (screenElement.textContent === "0" && screenElement.textContent !== "00") {
+    screenElement.textContent = "5";
+  } else {
+    screenElement.textContent += "5";
+  }
+}
+function sixPressed(e) {
+  if (screenElement.textContent === "0" && screenElement.textContent !== "00") {
+    screenElement.textContent = "6";
+  } else {
+    screenElement.textContent += "6";
+  }
+}
+function sevenPressed(e) {
+  if (screenElement.textContent === "0" && screenElement.textContent !== "00") {
+    screenElement.textContent = "7";
+  } else {
+    screenElement.textContent += "7";
+  }
+}
+function eightPressed(e) {
+  if (screenElement.textContent === "0" && screenElement.textContent !== "00") {
+    screenElement.textContent = "8";
+  } else {
+    screenElement.textContent += "8";
+  }
+}
+function ninePressed(e) {
+  if (screenElement.textContent === "0" && screenElement.textContent !== "00") {
+    screenElement.textContent = "9";
+  } else {
+    screenElement.textContent += "9";
+  }
+}
 
 console.log(screenElement);
 console.log(resetButtonElement);
